85ae5d0916bf848639c7676df87cc047b4ee25b0
lucas <dantaslucas337@gmail.com>
2022-07-12 13:38:54 -0300

    Regras de nomeclatura adicionadas.

--- Projeto módulo 1/src/br/com/generation/EuroeRubro/Euro_e_Rubro.java
+++ Projeto módulo 1/src/br/com/generation/EuroeRubro/Euro_e_Rubro.java
@@ -1,5 +1,13 @@
 package br.com.generation.EuroeRubro;
 
 public class Euro_e_Rubro {
+	/*Lembre-se, a declaração da variavel deve seguir o seguinte modelo:
+	 
+	  "double reaBR" 
+	  
+	  Sendo rea- tres primeiras letras do nome da moeda, 
+	  e BR o pais base da onde ela é usada; Bom trabalho a todos!
+	  
+	*/
 
 }
