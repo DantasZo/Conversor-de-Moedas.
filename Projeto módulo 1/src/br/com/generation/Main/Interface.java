18e7d3edc5977d2bd048296b40844214f0a6d7f6
Vitor Martins <vitormartinsxd@outlook.com>
2022-07-12 18:15:25 -0300

    Interface em progresso

--- Projeto módulo 1/src/br/com/generation/Main/Interface.java
+++ Projeto módulo 1/src/br/com/generation/Main/Interface.java
@@ -1,13 +1,24 @@
 package br.com.generation.Main;
 
 public class Interface {
-	/*Lembre-se, a declaração da variavel deve seguir o seguinte modelo:
-	 
-	  "double reaBR" 
-	  
-	  Sendo rea- tres primeiras letras do nome da moeda, 
-	  e BR o pais base da onde ela é usada; Bom trabalho a todos!
-	  Branch-Interface
-	*/
-
+	
+	public static void main(String[] args) {
+		
+	
+		System.out.println();
+		System.out.println();
+		System.out.println("     ╔═════════════════════════════════════════════════════════════════════╗");
+		System.out.println("     ║                       Conversor de Moedas                           ║");
+		System.out.println("     ╚═════════════════════════════════════════════════════════════════════╝");
+		System.out.println("     ╔═════════════════════════════════════════════════════════════════════╗");
+		System.out.println("     ║                                                                     ║");
+		System.out.println("     ║                                                                     ║");
+		System.out.println("     ║                                                                     ║");
+		System.out.println("     ╚═════════════════════════════════════════════════════════════════════╝");
+		
+		
+		
+		
+		
+	}
 }
