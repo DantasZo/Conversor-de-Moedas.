fa120d3402a60d9b24c5e0893fb8e00aba41158e
Vitor Martins <vitormartinsxd@outlook.com>
2022-07-12 21:41:06 -0300

    Interface em fase final

---
+++ Projeto módulo 1/src/br/com/generation/Main/ConstrutorConversao.java
@@ -0,0 +1,22 @@
+package br.com.generation.Main;
+
+public class ConstrutorConversao {
+	private double Euro,Real;
+
+
+	public ConstrutorConversao(double real)
+	{
+		Real = 5.44;
+		Euro = 1;
+	}
+
+	public double getReal()
+	{
+		return + Real;
+	}
+		
+	public double getEuro()
+		{
+			return + Euro;
+	}
+}
--- Projeto módulo 1/src/br/com/generation/Main/Interface.java
+++ Projeto módulo 1/src/br/com/generation/Main/Interface.java
@@ -1,24 +1,71 @@
 package br.com.generation.Main;
 
+import java.util.Scanner;
+
 public class Interface {
-	
+
 	public static void main(String[] args) {
 		
+		Scanner entrada = new Scanner (System.in);
+		
+		ConstrutorConversao cp = new ConstrutorConversao(0);
+		
+		int usuario;
+		double valor;
+		System.out.println();
+		System.out.println("╔═════════════════════════════════════════════════════════════════════╗");
+		System.out.println("║                       Conversor de Moedas                           ║");
+		System.out.println("╚═════════════════════════════════════════════════════════════════════╝");
+		System.out.println("╔═════════════════════════════════════════════════════════════════════╗");
+		System.out.println("║                    Aplicando conversões de moedas                   ║");
+		System.out.println("║                                                                     ║");
+		System.out.println("╚═════════════════════════════════════════════════════════════════════╝");
 	
-		System.out.println();
-		System.out.println();
-		System.out.println("     ╔═════════════════════════════════════════════════════════════════════╗");
-		System.out.println("     ║                       Conversor de Moedas                           ║");
-		System.out.println("     ╚═════════════════════════════════════════════════════════════════════╝");
-		System.out.println("     ╔═════════════════════════════════════════════════════════════════════╗");
-		System.out.println("     ║                                                                     ║");
-		System.out.println("     ║                                                                     ║");
-		System.out.println("     ║                                                                     ║");
-		System.out.println("     ╚═════════════════════════════════════════════════════════════════════╝");
+		System.out.println("\nMoedas disponiveis para conversão  " );
+		System.out.println("═══════════════════════════════════════════════════════════════════════");
+		System.out.println("\n1- Digite 1 para Dolar" + "\n2- Digite 2 para Euro" + "\n3- Digite 3 para Rubro" + 
+						   "\n4- Digite 4 para Iene" + "\n5- Digite 5 para Libra Esterlina" + "\n6- Digite 6 para Renminbi" +
+						   "\n7- Digite 7 para Real" + "\n8- Digite 8 para Peso Argentino" + "\n9- Digite 9 para Sair");
+		System.out.println("═══════════════════════════════════════════════════════════════════════");
+		usuario = entrada.nextInt();
 		
+		System.out.println("Digite o valor: ");
+		valor = entrada.nextDouble();
 		
+		if (usuario == 1) {
+			System.out.println("As conversões do Dolar para outras moedas são: ");
+			System.out.println("Real: " + valor * cp.getReal());
+			System.out.println("Euro: " + valor * cp.getEuro());
+		}
+		if (usuario == 2) {
+			System.out.println("As conversões do Euro para outras moedas são: ");
+		}
+		if (usuario == 3) {
+			System.out.println("As conversões do Rubro para outras moedas são: ");
+		}
+		if (usuario == 4) {
+			System.out.println("As conversões do Iene para outras moedas são: ");
+		}
+		if (usuario == 5) {
+			System.out.println("As conversões do Libra Esterlina para outras moedas são: ");
+		}
+		if (usuario == 6) {
+			System.out.println("As conversões do Renminbi para outras moedas são: ");
+		}
+		if (usuario == 7) {
+			System.out.println("As conversões do Real para outras moedas são: ");
+		}
+		if (usuario == 8) {
+			System.out.println("As conversões do Peso Argentino para outras moedas são: ");
+		}
+		if (usuario == 9) {
+			System.out.println("Fim de programa");
+			
+		}
+		if (usuario > 9 || usuario < 1 ) {
+			System.out.println("Digitou errado, tente novamente...");
+		}
 		
-		
-		
+		entrada.close();
 	}
 }
