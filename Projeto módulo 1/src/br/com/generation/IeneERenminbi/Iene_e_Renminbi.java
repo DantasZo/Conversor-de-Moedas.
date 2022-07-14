7c25a53f46c616e2b5453d6319208e7ce1fbfb74
lucas <dantaslucas337@gmail.com>
2022-07-14 13:30:19 -0300

    Atualização de metódos

--- Projeto módulo 1/src/br/com/generation/IeneERenminbi/Iene_e_Renminbi.java
+++ Projeto módulo 1/src/br/com/generation/IeneERenminbi/Iene_e_Renminbi.java
@@ -1,13 +1,36 @@
 package br.com.generation.IeneERenminbi;
 
+import java.util.Scanner;
+
 public class Iene_e_Renminbi {
-	/*Lembre-se, a declaração da variavel deve seguir o seguinte modelo:
-	 
-	  "double reaBR" 
-	  
-	  Sendo rea- tres primeiras letras do nome da moeda, 
-	  e BR o pais base da onde ela é usada; Bom trabalho a todos!
-	  Branch-Iene
-	*/
+
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
+
+		double soma, fonte;
+		double dolEUA = 0.0073  ;
+		double realBR = 5.42;
+		double ienJAP;
+
+		System.out.println("Insira a quantidade de ienes: ");
+		ienJAP = leia.nextDouble();
+
+
+		soma = ienJAP*dolEUA;
+		fonte = soma;
+
+		System.out.println("Seu iene em dolar: "+soma);
+		soma = fonte*realBR;  
+		System.out.println("Essa é sua quantidade de iene em reais: "+soma);
+		soma = fonte*6.72;
+		System.out.println("Essa é sua quantidade de ienes em Renminbi: "+soma);
+		soma = fonte*0.84;
+		System.out.println("Essa sua quantidade de ienes em Libra Esterlina: "+soma);
+
+		leia.close();	
+	}
+
 
 }
--- Projeto módulo 1/src/br/com/generation/Main/Interface.java
+++ Projeto módulo 1/src/br/com/generation/Main/Interface.java
@@ -1,13 +1,31 @@
 package br.com.generation.Main;
 
 public class Interface {
-	/*Lembre-se, a declaração da variavel deve seguir o seguinte modelo:
-	 
-	  "double reaBR" 
-	  
-	  Sendo rea- tres primeiras letras do nome da moeda, 
-	  e BR o pais base da onde ela é usada; Bom trabalho a todos!
-	  Branch-Interface
-	*/
+	private double Euro,Real, LibEst,Rublo;
 
+
+	public Interface(double real)
+	{
+		Real = 5.38;
+		Euro = 0.99;
+		LibEst = 0.89;
+		Rublo = 58.48;
+		
+	}
+
+	public double getReal() {
+		return + Real;
+	}
+		
+	public double getEuro() {
+			return + Euro;
+	}
+	
+	public double getLibra() {
+		return + LibEst;
+	}
+	
+	public double getRublo() {
+		return + Rublo;
+	}
 }
