1f09ff4ce198dcc3e50111ff28d60c77101b513f
lucas <dantaslucas337@gmail.com>
2022-07-15 12:44:44 -0300

    Adaptado valores moeda real

--- Projeto módulo 1/src/br/com/generation/RealEPesoargentino/Real.java
+++ Projeto módulo 1/src/br/com/generation/RealEPesoargentino/Real.java
@@ -1,39 +1,45 @@
 package br.com.generation.RealEPesoargentino;
-
-import java.text.DecimalFormat;
-import java.util.Scanner;
-
 public class Real {
 
-	public static void main(String[] args) {
+	private double dolUSA, libUK, dolCAN, eurGER, rubRUS, ienJAP, remCHI, pesARG;
 
-		Scanner leia = new Scanner(System.in);
+	public Real() {
 
+		super();
 
-		double soma = 0,rubRUS = 10.76, dolCAN = 0.24, eurGER =  0.18;
-		double dolUSA = 0.18 , reaBR = 0, remCHI = 1.25 , libUK = 0.16 , ienJAP = 25.61;
-        DecimalFormat form = new DecimalFormat();
-		System.out.println("Insira a quantidade de Reais: ");
-		reaBR = leia.nextDouble();
-
-		soma = reaBR * dolUSA;
-
-		System.out.println("Seu Real em dolar: "+form.format(soma));
-		soma = reaBR*ienJAP;  
-		System.out.println("Essa é sua quantidade de Real em iene: "+soma);
-		soma = reaBR * libUK;
-				System.out.println("Essa é sua quantidade de Real em Esterlina: "+soma);
-		soma = reaBR * remCHI;
-				System.out.println("Essa sua quantidade de Real em Remimbi: "+soma);
-		soma = reaBR * eurGER;
-				System.out.println("Essa sua quantidade de Real em Euro: "+form.format(soma));
-		soma = reaBR * dolCAN;
-				System.out.println("Essa sua quantidade de Real em Dolar Canadense: "+soma);
-		soma = reaBR * rubRUS;
-				System.out.println("Essa sua quantidade de Real em Rublo Russo: "+soma);
-		soma = reaBR * libUK;
-				System.out.println("Essa sua quantidade de Real em Libra Esterlina: "+soma);
-
-		leia.close();	
+		dolUSA = 0.19;
+		libUK = 0.16;
+		dolCAN = 0.24;
+		eurGER = 0.18;
+		rubRUS = 10.76;
+		ienJAP = 25.61;
+		remCHI = 1.25;
+		pesARG = 23.83;
 	}
+	
+	public double getEuro() {
+		return + eurGER;
+	}
+	public double getLibra() {
+		return + libUK;
+	}
+	public double getRublo() {
+		return + rubRUS;
+	}
+	public double getdolCAN() {
+		return + dolCAN;
+	}
+	public double getdolUSA() {
+		return + dolUSA;
+	}
+	public double getienJAP() {
+        return + ienJAP;
+	}
+	public double getdoremCHIA() {
+         return + remCHI;
+	}
+	public double getpesARG() {
+          return + pesARG;
+	}
+
 }
